7f3ec3987c30f11d3f8506f6576e9f361f90aa1c
Peihao <893109628@qq.com>
2021-11-18 18:56:10 -0800

    initialize project

---
+++ SeattleDashboard/src/main/java/com/example/seattledashboard/SeattleDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.seattledashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeattleDashboardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SeattleDashboardApplication.class, args);
+  }
+
+}
---
+++ SeattleDashboard/src/test/java/com/example/seattledashboard/SeattleDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seattledashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeattleDashboardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
